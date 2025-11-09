b521cb18c70321a5c08665f456522bd964f35583
Arthur Franco <153031237+Ima23-d@users.noreply.github.com>
2025-11-09 00:50:25 -0300

    Update config.py
    Signed-off-by: Arthur Franco <153031237+Ima23-d@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -17,9 +17,26 @@ def conectar():
             port=os.getenv("DB_PORT"),
             user=os.getenv("DB_USER")
         )
-        print("Conectado ao banco de dados com sucesso!")
+        print("Conectado ao banco de dados!")
         return conn
-
     except Exception as e:
-        print(f"Erro ao se conectar: {e}")
+        print(f"Erro ao conectar: {e}")
         return None
+
+def criar_tabelas():
+    conn = conectar()
+    if conn:
+        cursor = conn.cursor()
+        try:
+            with open("criar_tabelas.sql", "r", encoding="utf-8") as file:
+                sql_script = file.read()
+            cursor.execute(sql_script)
+            conn.commit()
+            print("Script SQL executado com sucesso na nuvem!")
+        except FileNotFoundError as e:
+            print(f"Erro: Arquivo SQL não encontrado. ({e})")
+        except Exception as e:
+            print(f"Erro ao executar script SQL: {e}")
+        finally:
+            cursor.close()
+            conn.close()
